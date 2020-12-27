9cab83d780610971943f4297dbe18b69f8fefda4
nalexeev <nalexeev@sharplogic.ru>
2020-12-27 17:26:51 +0900

    Add npgsql and ngx-webcam

---
+++ VideoChat.Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace VideoChat.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+             : base(options)
+        {
+        }
+    }
+}
