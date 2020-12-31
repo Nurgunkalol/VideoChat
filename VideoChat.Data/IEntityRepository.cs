576f4ba6184049d65d272816569815809faf3900
nalexeev <nalexeev@sharplogic.ru>
2021-01-01 04:44:35 +0900

    Add repository, unit of work

---
+++ VideoChat.Data/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VideoChat.Data
+{
+    public interface IEntityRepository<TEntity> where TEntity: class
+    {
+        IQueryable<TEntity> GetEntities();
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        Task<List<TEntity>> ToListAsync(IQueryable<TEntity> queryable, CancellationToken cancellationToken = default);
+        Task<TEntity> FirstOrDefaultAsync(IQueryable<TEntity> queryable, CancellationToken cancellationToken = default);
+    }
+}
